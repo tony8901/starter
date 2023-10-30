71b604a61acf03973bb0b58a43ca107d9be281f6
Tony <82529853+tony8901@users.noreply.github.com>
2023-10-30 09:56:19 -0400

    change StudentQueryResponse initialization

--- src/microservices/student/src/main/java/com/microservice/student/application/http/StudentQueryResponse.java
+++ src/microservices/student/src/main/java/com/microservice/student/application/http/StudentQueryResponse.java
@@ -1,13 +1,23 @@
 package com.microservice.student.application.http;
 
+import com.microservice.student.domain.Student;
 import com.microservice.utils.core.IRequestResponse;
+import lombok.Getter;
 
 import java.util.List;
 import java.util.UUID;
 
-public record StudentQueryResponse(
-        UUID id,
-        String name,
-        String email,
-        List<UUID> courses) implements IRequestResponse {
+@Getter
+public class StudentQueryResponse implements IRequestResponse {
+    private final UUID id;
+    private final String name;
+    private final String email;
+    private final List<UUID> courses;
+
+    public StudentQueryResponse(Student student) {
+        this.id = student.id();
+        this.name = student.name();
+        this.email = student.email();
+        this.courses = student.courses();
+    }
 }
